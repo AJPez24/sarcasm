b799c5776663a3c9cc1c82549ee69f879526da09
allievor <allievorley@gmail.com>
2025-11-16 15:40:01 +0100

    Commit without Anaconda installer file

--- allietest.py
+++ allietest.py
@@ -1,1 +1,12 @@
-print("test")
+# data exploration
+
+# use ijson parser for large json file
+import ijson
+
+# json file is structured as a dictionary - go through key value pairs
+count = 0
+with open("comments.json", "rb") as f:
+    for key, value in ijson.kvitems(f, ""):
+        count += 1
+
+print("Total items:", count) # 12704751 items
