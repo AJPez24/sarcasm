5e4f445684c417f6ec02055a0465a67555dd0a9b
AJPez24 <anthony.j.pezza@gmail.com>
2025-11-18 09:21:15 +0100

    working bert embeddings test

--- bert-test/embeddings.py
+++ bert-test/embeddings.py
@@ -1,10 +1,14 @@
-from transformers import BertTokenizer, TFBertModel
+from transformers import BertTokenizer, BertModel
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-model = TFBertModel.from_pretrained("bert-base-uncased")
+model = BertModel.from_pretrained("bert-base-uncased")
 
 text = "Replace me by any text you'd like."
 
-encoded_input = tokenizer(text, return_tensors='tf')
+encoded_input = tokenizer(text, return_tensors='pt')
 
 output = model(**encoded_input)
+
+embeddings = output.last_hidden_state
+
+print(embeddings.shape)
